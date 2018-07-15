535bd8358e8a73174cb2be7892cbbf38fd4b885f
kazuma.endo <diamond_peanuts@yahoo.co.jp>
2018-07-15 20:39:15 +0900

    Add NewServlet

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -32,7 +32,7 @@ public class NewServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	    request.setAttribute("_token", request.getSession().getId());
-        request.setAttribute("taske", new Task());
+        request.setAttribute("task", new Task());
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
         rd.forward(request, response);
